9c4ce058d6609d4bec19bf8444ee8777b54fe6c8
GregOSF <gregory.onza@gmail.com>
2016-07-01 00:26:23 -0700

    jquery for modal functionality

--- scripts.js
+++ scripts.js
@@ -1,10 +1,8 @@
 console.log("Hello world");
+
+
+
 $(function(){
-
-	$
-
-
-
 
 	var products = {
 	1: {
@@ -12,41 +10,82 @@ $(function(){
 	    order: 1,
 	    'title' : 'Product Title 1',
 	    'price' : 'Staring at $500',
-	    'desc' : 'Cras a ante eget dui dignissim molestie nec non lectus. Donec id sapien tristique, iaculis odio a, gravida ligula. In hac habitasse platea dictumst. Praesent non lacus elit. Sed convallis erat sem, ac egestas ipsum tempor in. Sed consectetur ex at dolor ullamcorper vulputate. Ut tristique ipsum velit, eget pretium ipsum dictum vel. Aenean ut tempus elit, non facilisis odio. Vivamus hendrerit ipsum vel risus ultrices, ut ornare metus ornare. Quisque semper ante ac pellentesque vestibulum. Cras a massa turpis. Pellentesque congue leo velit, non porta dui euismod nec.<br> <br> Mauris sed enim risus. Quisque mollis justo quis commodo euismod. Nullam non massa in lorem cursus sagittis. Sed laoreet felis mauris, a eleifend sapien molestie eget. Duis at volutpat elit. Phasellus sit amet erat ut libero lobortis sollicitudin pellentesque ac lectus. Phasellus venenatis ultrices justo nec venenatis.<br><br>'
-	        +'<ul>'
-	                + '     <li>Non porta dui euismod nec</li>'
-	                + '     <li>Ut tristique ipsum velit</li>'
-	                                + '     <li>Quisque mollis justo</li>'
-	        +'</ul>',
-	    'uri-stub'   : '/buy-mattress/iseries/iseries-hybrid/iseries-approval-super-pillow-top',
-	                    'src'        : 'material/000010885.jpg'
+	    'desc' : 'Cras a ante eget dui dignissim molestie nec non lectus. Donec id sapien tristique, iaculis odio a, gravida ligula. In hac habitasse platea dictumst. Praesent non lacus elit. Sed convallis erat sem, ac egestas ipsum tempor in. Sed consectetur ex at dolor ullamcorper vulputate. Ut tristique ipsum velit, eget pretium ipsum dictum vel. Aenean ut tempus elit, non facilisis odio. Vivamus hendrerit ipsum vel risus ultrices, ut ornare metus ornare. Quisque semper ante ac pellentesque vestibulum. Cras a massa turpis. Pellentesque congue leo velit, non porta dui euismod nec.<br> <br> Mauris sed enim risus. Quisque mollis justo quis commodo euismod. Nullam non massa in lorem cursus sagittis. Sed laoreet felis mauris, a eleifend sapien molestie eget. Duis at volutpat elit. Phasellus sit amet erat ut libero lobortis sollicitudin pellentesque ac lectus. Phasellus venenatis ultrices justo nec venenatis.',
+	    'feature1' : 'Non porta dui euismod nec',
+	    'feature2' : 'Ut tristique ipsum velit',
+	    'feature3' : 'Quisque mollis justo',
+	    'feature4' : 'Nullam non massa in lorem',
+	    'feature5' : 'Phasellus venenatis ultrices justo',
+	    'feature6' : 'uisque mollis justo quis commodo',
+	    'img'   : '/assets/images/cereal_b.jpg',
+	    'src'   : 'material/000010885.jpg'
 	},
 	2: {
 	    id : 2,
 	    order: 2,
 	    'title' : 'Product Title 2',
 	    'price' : 'Staring at $1900',
-	    'desc' : 'Cras a ante eget dui dignissim molestie nec non lectus. Donec id sapien tristique, iaculis odio a, gravida ligula. In hac habitasse platea dictumst. Praesent non lacus elit. Sed convallis erat sem, ac egestas ipsum tempor in. Sed consectetur ex at dolor ullamcorper vulputate. Ut tristique ipsum velit, eget pretium ipsum dictum vel. Aenean ut tempus elit, non facilisis odio. Vivamus hendrerit ipsum vel risus ultrices, ut ornare metus ornare. Quisque semper ante ac pellentesque vestibulum. Cras a massa turpis. Pellentesque congue leo velit, non porta dui euismod nec.<br> <br> Mauris sed enim risus. Quisque mollis justo quis commodo euismod. Nullam non massa in lorem cursus sagittis. Sed laoreet felis mauris, a eleifend sapien molestie eget. Duis at volutpat elit. Phasellus sit amet erat ut libero lobortis sollicitudin pellentesque ac lectus. Phasellus venenatis ultrices justo nec venenatis.',
-	    'uri-stub'   : '/buy-mattress/iseries/iseries-hybrid/iseries-approval-super-pillow-top',
-	                    'src'        : 'material/ff_x20_008.JPG'
+	    'desc' : 'Cras a ante eget dui dignissim molestie nec non lectus. Donec id sapien tristique, iaculis odio a, gravida ligula. In hac habitasse platea dictumst. Praesent non lacus elit. Sed convallis erat sem, ac egestas ipsum tempor in. Sed consectetur ex at dolor ullamcorper vulputate. Ut tristique ipsum velit, eget pretium ipsum dictum vel. Aenean ut tempus elit, non facilisis odio. Vivamus hendrerit ipsum vel risus ultrices, ut ornare metus ornare. Quisque semper ante ac pellentesque vestibulum. Cras a massa turpis. Pellentesque congue leo velit, non porta dui euismod nec.',
+	    'feature1' : 'Mauris sed enim risus',
+	    'feature2' : 'Quisque mollis justo quis commodo euismod',
+	    'feature3' : 'Nullam non massa in lorem cursus sagittis',
+	    'feature4' : 'Sed laoreet felis mauris, a eleifend sapien molestie eget',
+	    'feature5' : 'Duis at volutpat elit',
+	    'feature6' : 'Phasellus sit amet erat ut libero lobortis sollicitudin pellentesque ac lectus',
+	    'img'   : '/assets/images/flower_b.jpg',
+	    'src'   : 'material/ff_x20_008.JPG'
 	},
 	3: {
 	    id : 3,
 	    order: 3,
 	    'title' : 'Product Title 3',
 	    'price' : 'Staring at $2730',
-	    'desc' : 'Cras a ante eget dui dignissim molestie nec non lectus. Donec id sapien tristique, iaculis odio a, gravida ligula. In hac habitasse platea dictumst. Praesent non lacus elit. Sed convallis erat sem, ac egestas ipsum tempor in. Sed consectetur ex at dolor ullamcorper vulputate. Ut tristique ipsum velit, eget pretium ipsum dictum vel. Aenean ut tempus elit, non facilisis odio. Vivamus hendrerit ipsum vel risus ultrices, ut ornare metus ornare. Quisque semper ante ac pellentesque vestibulum. Cras a massa turpis. Pellentesque congue leo velit, non porta dui euismod nec.<br> <br> Mauris sed enim risus. Quisque mollis justo quis commodo euismod. Nullam non massa in lorem cursus sagittis. Sed laoreet felis mauris, a eleifend sapien molestie eget. Duis at volutpat elit. Phasellus sit amet erat ut libero lobortis sollicitudin pellentesque ac lectus. Phasellus venenatis ultrices justo nec venenatis.',
-	    'uri-stub'   : '/buy-mattress/iseries/iseries-hybrid/iseries-approval-super-pillow-top',
-	                    'src'        : 'material/sample1.jpg'
+	    'desc' : 'Cras a ante eget dui dignissim molestie nec non lectus. Donec id sapien tristique, iaculis odio a, gravida ligula. In hac habitasse platea dictumst. Praesent non lacus elit. Sed convallis erat sem, ac egestas ipsum tempor in. Sed consectetur ex at dolor ullamcorper vulputate. Ut tristique ipsum velit, eget pretium ipsum dictum vel. Aenean ut tempus elit, non facilisis odio. Vivamus hendrerit ipsum vel risus ultrices, ut ornare metus ornare. Quisque semper ante ac pellentesque vestibulum. Cras a massa turpis. Pellentesque congue leo velit, non porta dui euismod nec.',
+	    'feature1' : 'Mauris sed enim risus',
+	    'feature2' : 'Quisque mollis justo quis commodo euismod',
+	    'feature3' : 'Nullam non massa in lorem cursus sagittis',
+	    'feature4' : 'Sed laoreet felis mauris, a eleifend sapien molestie eget',
+	    'feature5' : 'Duis at volutpat elit',
+	    'feature6' : 'Phasellus sit amet erat ut libero lobortis sollicitudin pellentesque ac lectus',
+	    'img'   : 'assets/images/machine_b.jpg',
+	    'src'   : 'material/sample1.jpg'
 	},
 	4: {
 	    id : 4,
 	    order: 4,
 	    'title' : 'Product Title 3',
 	    'price' : 'Staring at $731',
-	    'desc' : 'Cras a ante eget dui dignissim molestie nec non lectus. Donec id sapien tristique, iaculis odio a, gravida ligula. In hac habitasse platea dictumst. Praesent non lacus elit. Sed convallis erat sem, ac egestas ipsum tempor in. Sed consectetur ex at dolor ullamcorper vulputate. Ut tristique ipsum velit, eget pretium ipsum dictum vel. Aenean ut tempus elit, non facilisis odio. Vivamus hendrerit ipsum vel risus ultrices, ut ornare metus ornare. Quisque semper ante ac pellentesque vestibulum. Cras a massa turpis. Pellentesque congue leo velit, non porta dui euismod nec.<br> <br> Mauris sed enim risus. Quisque mollis justo quis commodo euismod. Nullam non massa in lorem cursus sagittis. Sed laoreet felis mauris, a eleifend sapien molestie eget. Duis at volutpat elit. Phasellus sit amet erat ut libero lobortis sollicitudin pellentesque ac lectus. Phasellus venenatis ultrices justo nec venenatis.',
-	    'uri-stub'   : '/buy-mattress/iseries/iseries-hybrid/iseries-approval-super-pillow-top',
-	                    'src'        : 'material/sample_08.jpg'
+	    'desc' : 'Cras a ante eget dui dignissim molestie nec non lectus. Donec id sapien tristique, iaculis odio a, gravida ligula. In hac habitasse platea dictumst. Praesent non lacus elit. Sed convallis erat sem, ac egestas ipsum tempor in. Sed consectetur ex at dolor ullamcorper vulputate. Ut tristique ipsum velit, eget pretium ipsum dictum vel. Aenean ut tempus elit, non facilisis odio. Vivamus hendrerit ipsum vel risus ultrices, ut ornare metus ornare. Quisque semper ante ac pellentesque vestibulum. Cras a massa turpis. Pellentesque congue leo velit, non porta dui euismod nec.',
+	    'feature1' : 'Mauris sed enim risus',
+	    'feature2' : 'Quisque mollis justo quis commodo euismod',
+	    'feature3' : 'Nullam non massa in lorem cursus sagittis',
+	    'feature4' : 'Sed laoreet felis mauris, a eleifend sapien molestie eget',
+	    'feature5' : 'Duis at volutpat elit',
+	    'feature6' : 'Phasellus sit amet erat ut libero lobortis sollicitudin pellentesque ac lectus',
+	    'img'   : '/assets/images/candy_b.jpg',
+	    'src'   : 'material/sample_08.jpg'
 	}
 	};
+
+	// Get the modal
+	$('#productModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var recipient = button.data('id') // Extract info from data-* attributes
+  console.log(recipient)
+  console.log(products[recipient].title)
+  console.log(recipient)
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var modal = $(this)
+  modal.find('.modal-title').text(products[recipient].title)
+  modal.find('.modal-price').text(products[recipient].price)
+  modal.find('.modal-desc').text(products[recipient].desc)
+  modal.find('.modal-img').text(products[recipient].img)
+  modal.find('.modal-body input').val(recipient)
+  $(".modal-img").attr("src", products[recipient].img);
+})
+
+
+
+
 });
